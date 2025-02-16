12a2b0ba8ef95f6370782e1553d5a5dd18d8870c
Dr. Peter Netz <152088022+zentrocdot@users.noreply.github.com>
2025-02-16 15:25:27 +0100

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,16 +1,16 @@
 # Import the Python modules of the node.
 from .nodes.qr_codes import *
-from .nodes.qr_codes_circular import *
+from .nodes.qr_codes_style_color import *
 from .nodes.qr_codes_reader import *
 from .nodes.show_data import *
 from .nodes.qr_codes_segno import *
 from .nodes.qr_codes_segno_simple import *
-#from .nodes.qr_codes_segno_image import *
+from .nodes.qr_codes_segno_logo import *
 
 NODE_CLASS_MAPPINGS = {
     "🛸 QRCodes (Segno Full Version)": QRCodesSegnoFull,
     "🛸 QRCodes (Segno Simple Version)": QRCodesSegnoSimple,
-    #"🛸 QRCodes (Segno Simple Logo)": QRCodesSegnoLogo,
+    "🛸 QRCodes (Segno Simple Logo)": QRCodesSegnoLogo,
     "🛰️ QRCodes (Simple Color)": QRCodesSimple,
     "🛰️ QRCodes (Simple Logo)": QRCodesLogo,
     "🛰️ QRCodes (Simple Style)": QRCodesStyle,
